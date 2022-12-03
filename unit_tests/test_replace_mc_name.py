88d4b54a1a3246beb60da87e1bbef84559951bf9
EunjeongHur <ungdunge2@gmail.com>
2022-12-02 23:47:09 -0800

    Complete test_replace_mc_name.py

--- unit_tests/test_replace_mc_name.py
+++ unit_tests/test_replace_mc_name.py
@@ -3,5 +3,14 @@ from game import replace_mc_name
 
 
 class TestReplaceMcName(TestCase):
-    def test_replace_mc_name(self):
-        self.fail()
+    def test_replace_mc_name_one_item(self):
+        name = 'Alice'
+        lines = ['Hello, /mc_name']
+        expected = ['Hello, \033[92mAlice\033[00m']
+        self.assertEqual(expected, replace_mc_name(name, lines))
+
+    def test_replace_mc_name_many_items(self):
+        name = 'Kitty'
+        lines = ['My name is /mc_name', '/mc_name is doing test']
+        expected = ['My name is \033[92mKitty\033[00m', '\033[92mKitty\033[00m is doing test']
+        self.assertEqual(expected, replace_mc_name(name, lines))
