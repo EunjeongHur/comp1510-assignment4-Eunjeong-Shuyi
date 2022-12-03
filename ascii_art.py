d08c02a144915057ec2e2e38fc4965e3e6529c2c
EunjeongHur <ungdunge2@gmail.com>
2022-12-02 20:46:05 -0800

    Add ascii art of cat in ascii_art.py file

---
+++ ascii_art.py
@@ -0,0 +1,101 @@
+"""
+Member #1 Student Name: Alice Hur
+Member #1 Student Number: A01272954
+
+Member #2 Student Name: Shuyi Liu
+Member #2 Student Number: A01178380
+"""
+
+
+def sleeping_cat_art():
+    # By 'Felix Lee'
+    print("      |\\      _,,,---,,_")
+    print("ZZZzz /,`.-'`'    -.  ;-;;,_")
+    print("     |,4-  ) )-,_. ,\\ (  `'-'")
+    print("    '---''(_/--'  `-'\\_)")
+
+
+def two_cats_art():
+    # By 'KAT'
+    print("                      /^--^\\     /^--^\\  ")
+    print("                      \\____/     \\____/  ")
+    print("                     /      \\   /      \\ ")
+    print("                    |        | |        |")
+    print("                     \\__  __/   \\__  __/")
+    print("|^|^|^|^|^|^|^|^|^|^|^|^\\ \\^|^|^|^/ /^|^|^|^|^|^|^|^|^|^|")
+    print("| | | | | | | | | | | | |\\ \\| | |/ /| | | | | | | | | | |")
+    print("########################/ /######\\ \\#####################")
+    print("| | | | | | | | | | | | \\/| | | | \\/| | | | | | | | | | |")
+    print("|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|")
+
+
+def poor_kitty_art():
+    # By 'Shanaka Dias'
+    print("           ___")
+    print("          (___)")
+    print("   ____")
+    print(" _\\___ \\  |\\_/|")
+    print("\\     \\ \\/ , , \\ ___")
+    print(" \\__   \\ \\ =\"= //|||\\ ")
+    print("  |===  \\/____)_)||||")
+    print("  \\______|    | |||||")
+    print("      _/_|  | | =====")
+    print("     (_/  \\_)_)")
+    print("  _________________")
+    print(" (                _)")
+    print("  (__   '          )")
+    print("    (___    _____)")
+    print("        '--'")
+
+
+def lonely_cat_art():
+    # By 'hjw'
+    print("         *                  *")
+    print("             __                *")
+    print("          ,db'    *     *")
+    print("         ,d8/       *        *    *")
+    print("         888")
+    print("         `db\\       *     *")
+    print("           `o`_                    **")
+    print("      *               *   *    _      *")
+    print("            *                 / )")
+    print("         *    (\\__/) *       ( (  *")
+    print("       ,-.,-.,)    (.,-.,-.,-.) ).,-.,-.")
+    print("      | @|  ={      }= | @|  / / | @|o |")
+    print("     _j__j__j_)     `-------/ /__j__j__j_")
+    print("     ________(               /___________")
+    print("      |  | @| \\              || o|O | @|")
+    print("      |o |  |,'\\       ,   ,'\"|  |  |  |  ")
+    print("     vV\\|/vV|`-'\\  ,---\\   | \\Vv\\//vVv\\//v")
+    print("                _) )    `. \\ /")
+    print("               (__/       ) )")
+    print("                         (_/")
+
+
+def finding_food_cat_art():
+    # by 'hjw'
+    print("                                     ,")
+    print("              ,-.       _,---._ __  / \\ ")
+    print("             /  )    .-'       `./ /   \\ ")
+    print("            (  (   ,'            `/    /|")
+    print("             \\  `-\"             \\'\\   / |")
+    print("              `.              ,  \\ \\ /  |")
+    print("               /`.          ,'-`----Y   |")
+    print("              (            ;        |   '")
+    print("              |  ,-.    ,-'         |  /")
+    print("              |  | (   |            | /")
+    print("              )  |  \\  `.___________|/")
+    print("              `--'   `--'")
+
+
+def cozy_cat_art():
+    # By 'Susie Oviatt'
+    print("               /\\____/\\    __")
+    print("             .'  \"\"\"\"  `,-'  `--.__")
+    print("        __,- :   -  -  ;  \" ::     `-. -.__")
+    print("     ,-sssss `._  `' _,'\"     ,'~~~::`.sssss-.")
+    print("    |ssssss ,' ,_`--'_    __,' ::  `  `.ssssss|")
+    print("   |sssssss `-._____~ `,,'_______,---_;; ssssss|")
+    print("    |ssssssssss     `--'~{__   ____   ,'ssssss|")
+    print("     `-ssssssssssssssssss ~~~~~~~~~~~~ ssss.-'")
+    print("          `---.sssssssssssssssssssss.---'")
