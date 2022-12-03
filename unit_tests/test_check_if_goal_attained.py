f440a2ffd8e78de5072378f1ede24e5ab77b49dc
EunjeongHur <ungdunge2@gmail.com>
2022-12-02 23:36:47 -0800

    Modify test name in test_check_if_goal_attained.py

--- unit_tests/test_check_if_goal_attained.py
+++ unit_tests/test_check_if_goal_attained.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from game import check_if_goal_attained
 
 
-class Test(TestCase):
+class TestCheckIfGoalAttained(TestCase):
     def test_check_if_goal_attained_nero_true(self):
         character = {'X-coordinate': 9, 'Y-coordinate': 4, 'Nero': [5, 20], 'Lulu': [1, 10], 'Noah': [1, 10],
                      'Penelope': [1, 10], 'Name': 'Alice'}
