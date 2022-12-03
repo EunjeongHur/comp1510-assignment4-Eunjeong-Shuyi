867a403c6151f248bb98cae5dd538b9ba94dcbb5
EunjeongHur <ungdunge2@gmail.com>
2022-12-03 02:16:57 -0800

    Add unit tests on test_event_option.py

--- unit_tests/test_event_option.py
+++ unit_tests/test_event_option.py
@@ -7,10 +7,29 @@ from game import event_option
 class EventOption(TestCase):
 
     @patch('random.choice', return_value='Nero')
-    def test_event_option_penelope(self, mock_input):
+    def test_event_option_nero(self, random_value):
         character = {'X-coordinate': 9, 'Y-coordinate': 4, 'Nero': [5, 20], 'Lulu': [1, 10], 'Noah': [1, 10],
                      'Penelope': [1, 10], 'Name': 'Chris'}
         actual = event_option(character)
         self.assertEqual('Nero', actual)
 
-# not working
+    @patch('random.choice', return_value='Lulu')
+    def test_event_option_lulu(self, random_value):
+        character = {'X-coordinate': 9, 'Y-coordinate': 4, 'Nero': [5, 20], 'Lulu': [1, 10], 'Noah': [1, 10],
+                     'Penelope': [1, 10], 'Name': 'Chris'}
+        actual = event_option(character)
+        self.assertEqual('Lulu', actual)
+
+    @patch('random.choice', return_value='Noah')
+    def test_event_option_noah(self, random_value):
+        character = {'X-coordinate': 9, 'Y-coordinate': 4, 'Nero': [5, 20], 'Lulu': [1, 10], 'Noah': [1, 10],
+                     'Penelope': [1, 10], 'Name': 'Chris'}
+        actual = event_option(character)
+        self.assertEqual('Noah', actual)
+
+    @patch('random.choice', return_value='Penelope')
+    def test_event_option_penelope(self, random_value):
+        character = {'X-coordinate': 9, 'Y-coordinate': 4, 'Nero': [5, 20], 'Lulu': [1, 10], 'Noah': [1, 10],
+                     'Penelope': [1, 10], 'Name': 'Chris'}
+        actual = event_option(character)
+        self.assertEqual('Penelope', actual)
